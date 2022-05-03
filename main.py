d0224d1d2742d8e3c3a4a69b6f02f39dc16a5431
zethnest <16971676+zethnest@users.noreply.github.com>
2022-05-03 20:53:47 +0800

    refactor: Window class

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import urllib.parse
 from pprint import pprint
 from piratetui.TorrentItem import TorrentItem
 from piratetui.TorrentItems import TorrentItems
+from piratetui.Window import Window
 
 FILENAMES = {
     "mirror": "mirror.txt",
@@ -20,34 +21,6 @@ FILENAMES = {
     "lastsearch": "lastsearch.txt",
     "lastcategory": "lastcategory.txt",
 }
-
-
-
-
-class Window():
-    def __init__(self, window: 'curses._CursesWindow', border: bool):
-        self.window = window
-        self.border = border
-
-    def refresh(self):
-        self.window.refresh()
-
-    def clear(self):
-        self.window.clear()
-
-    def draw_border(self, ignore_property: bool = False):
-        if not self.border and not ignore_property:
-            return
-        rows, cols = self.window.getmaxyx()
-        for i in range(1, rows - 1):
-            self.window.addstr(i, 0, "│")
-            self.window.addstr(i, cols - 1, "│")
-        self.window.addstr(0       , 1       , "─" * (cols - 2))
-        self.window.addstr(rows - 1, 1       , "─" * (cols - 2))
-        self.window.addstr(0       , 0       , "┌")
-        self.window.addstr(0       , cols - 1, "┐")
-        self.window.addstr(rows - 1, 0       , "└")
-        self.window.insstr(rows - 1, cols - 1, "┘")
 
 
 def file_exists(path: str):
